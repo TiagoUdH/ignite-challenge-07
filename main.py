4697539697ca29f67625ec91eb086ebe6f6b9d53
Tiago Fernandes <tiagoferprojects@gmail.com>
2024-01-14 17:50:31 -0300

    Creating Menu

---
+++ main.py
@@ -0,0 +1,16 @@
+contacts = []
+
+while True:
+  print("\nLista de contatos:")
+  print("1. Adicionar contato")
+  print("2. Ver contatos")
+  print("3. Ediar contato")
+  print("4. Marcar/Desmarcar contato como favorito")
+  print("5. Listar contatos favoritos")
+  print("6. Apagar contato")
+  print("7. Sair\n")
+  
+  option = input("Digite a sua opção: ")
+  
+  if option == "7":
+    break
